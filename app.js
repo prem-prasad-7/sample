615ffb358f7784e4e0584bae0f6a7fd0877f4d78
prem-prasad-7 <727722euec131@skcet.ac.in>
2024-07-26 14:51:50 +0530

    commit

---
+++ app.js
@@ -0,0 +1,84 @@
+function myfunction(a,b)
+{
+    letsum = a+b
+    console.log(sum)
+}
+
+myfunction(5,6)
+
+const myFunction =(a,b)=>{
+    console.log(a,b)
+}
+
+myfunction("vetri","maaran")
+
+
+let arr = ["electronic","cloth","vegetable"]
+
+console.log(arr[1])
+    arr.map(data)=>(console.log(data))
+
+    let array1 = [12,19,16,21]
+
+    function checkEligibility(a){
+        return a>=18
+    }
+    console.log(array1.filter(checkEligibility))
+
+
+
+
+
+    let fruite=("Guava":2,"Litchi":4,"Mango":5)
+
+
+let value = fruits.Guava
+console.log
+
+const values ={
+    num2:0,
+    num1:1,
+    num3:2,
+
+    result:{
+
+    res: function(){
+        this.num2=10
+    }
+}
+},
+values.result.res()
+let num = values.result.num2
+console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+const data=fetch("")
+.then( (data)=>data.json()).then(res=>console.log(res[0].large))
+.catch(e=>console.log(e.message))
+
+
+
+
+
+
+let iteam = [("shirt":2,"T-shirt":3,"Langabab":5)]
+count data = iteam.map((iteam)=> item1)
+count data1=items.forEach((iteam)=>(iteam1))
+console.log(data)
+console.log(iteam)
+console.log()
+
+
+
+
+myFunction()
+const num 
